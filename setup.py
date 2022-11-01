f79a3ed3b8ad9d2ea9e060a4db915b1e73ae1d08
RaghuGulati <45628486+RaghuGulati@users.noreply.github.com>
2022-11-01 21:32:05 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,19 @@
-# TODO: Remove this file when bench >=v5.11.0 is adopted / v15.0.0 is released
-from setuptools import setup
+from setuptools import setup, find_packages
 
-name = "frappe"
+with open("requirements.txt") as f:
+    install_requires = f.read().strip().split("\n")
 
-setup()
+# get version from __version__ variable in test2/__init__.py
+from test2 import __version__ as version
+
+setup(
+    name="ems",
+    version=version,
+    description="event management system",
+    author="rg",
+    author_email="rg@rg.com",
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=install_requires
+)
